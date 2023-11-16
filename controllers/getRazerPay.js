1bc5953cd819c6ad811e93e784a0ad16a2c74b1b
PARTH CHAWLA <82279033+sponge-bobs-square-pants@users.noreply.github.com>
2023-11-17 01:21:34 +0530

    Delhivery api integration-0.0.3

--- controllers/getRazerPay.js
+++ controllers/getRazerPay.js
@@ -53,7 +53,7 @@ const getRazerPayController = async (req, res) => {
     // console.log('hello motto');
     const {phone,orderID,email,address,pincode,state,city,amount,name, userId, transactionID, cart} = req.body;
     const finalAmount = parseInt(amount)
-    console.log(cart);
+    const cartItemCount = cart.length;
     // console.log(finalAmount);
     const data = {  
         "merchantId":`${merchantaID}`,
@@ -63,7 +63,7 @@ const getRazerPayController = async (req, res) => {
         "amount":finalAmount,
         "merchantOrderId":`${orderID}`,
         "mobileNumber":`${phone}`,
-        "redirectUrl":`https://sonari-api.onrender.com/api/v1/verification?merchantId=${merchantaID}&transcationId=${transactionID}&merchantOrderId=${orderID}`,
+        "redirectUrl":`https://sonari-api.onrender.com/api/v1/verification?merchantId=${merchantaID}&transcationId=${transactionID}&merchantOrderId=${orderID}&cartItemCount=${cartItemCount}`,
         // "redirectUrl": `https://0676-194-61-40-52.ngrok.io/api/v1/verification?merchantId=${merchantaID}&transcationId=${transactionID}`,
         "redirectMode": "POST",
         "callbackUrl": "https://sonarinightwear.netlify.app/Products",
@@ -233,8 +233,8 @@ const exponentialBackoff = (retryCount) => Math.pow(2, retryCount) * 1000;
 const backendVerification = async (req, res) => {
    const merchantTransactionId=req.query.transactionId;
     const orderId=req.query.merchantOrderId;
-    // const cart = req.query.cart
-    // console.log(cart);
+    const cartItemCount = req.query.cartItemCount
+    console.log(cartItemCount);
    const merchantId=req.query.merchantId;
    const keyIndex = 1;
 //    const key = `${process.env.PHONE_PE_KEY}`;
